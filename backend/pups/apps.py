93c260902d69ae4b26b325f68e6548942d43c773
Sherklan <97853791+Sherklan12e@users.noreply.github.com>
2024-12-01 05:24:40 -0300

    kksz

--- backend/pups/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PupsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pups'
--- backend/pups/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import ContentItem
-
-class ContentItemSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ContentItem
-        fields = ['id', 'title', 'image', 'link', 'created_at', 'updated_at']
-        
